738297aec2bbea3e21b6fd98c888656e21e1f00f
mpaszkeiwicz <m.paszkiewicz@i-propert.pl>
2022-04-11 11:50:47 +0200

    suma

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var firstNumber = 4;
+var secondNumber = 6;
+function funkcja(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+
+}
+const suma = funkcja(firstNumber, secondNumber)
+console.log(suma);
+
+
+
